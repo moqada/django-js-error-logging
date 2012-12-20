943cea26ca77f63686d0e66d3564fb0f5b9e4421
moqada <moqada@gmail.com>
2012-12-20 11:28:58 +0900

    Fix a bug generating cache key at too long url

--- jserrorlogging/receivers.py
+++ jserrorlogging/receivers.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
+import hashlib
 from django.conf import settings
 from django.core.cache import cache
 from django.core.mail import send_mail
 from django.db.models.loading import get_model
 from django.template.loader import render_to_string
+from django.utils.encoding import iri_to_uri
 from .settings import (ENABLE_MODEL, LOG_MODEL,
                        ENABLE_EMAIL, MAIL_TO, MAIL_NOTIFY_INTERVAL)
 from .signals import add_log
+
+
+def _generate_cache_key(log_data):
+    """ generate cache key for notify_by_email
+    """
+    url = hashlib.md5(iri_to_uri(log_data['url']))
+    return u'jserrorlogging.email.%s:%s' % (url.hexdigest(), log_data['line'])
 
 
 def save_model(sender, **kwargs):
@@ -23,7 +32,7 @@ def notify_by_email(sender, **kwargs):
     """
     data = kwargs.get('data', {}).copy()
     data.update(meta=kwargs.get('meta', {}))
-    cache_key = u'jserrorlogging:%(url)s:%(line)s' % data
+    cache_key = _generate_cache_key(data)
     if cache.get(cache_key):
         return
     subject = render_to_string('jserrorlogging/email/subject.txt', dictionary=data)
